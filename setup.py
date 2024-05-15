10f12aaf670ce288645344f152d0387c74303eaa
Nits98cggit01 <145176851+Nits98cggit01@users.noreply.github.com>
2024-05-15 18:19:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 # setup.py
 
 from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Extracts visual table data from powerbi report'
+LONG_DESCRIPTION = 'A package that exports the visual table data into csv'
 
 setup(
     name='pbi_vizdata',
-    version='0.1',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
+    keywords=['python', 'powerbi','powerbiclient'],
     install_requires=[
         'os',
         'time',
@@ -13,7 +22,14 @@ setup(
         'shutil',
         're',
         'powerbiclient'
-    ],  # Add any dependencies here
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Powerbi Developers, Data analyst, Gen AI developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
     author = "Nitin Satish",
     zip_safe=False
 )
